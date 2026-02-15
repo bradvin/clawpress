6f650d94a907fb49223b550cb579f8b2a829551e
Brad Vincent <bradvin@gmail.com>
2026-02-15 09:38:04 +0000

    build: regenerate bundles and translations for setup rename

--- build/panel/panel.asset.php
+++ build/panel/panel.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '98b612638d4e16cb2016');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'b79edfad3a1774e01925');
--- build/scripts/admin.asset.php
+++ build/scripts/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'b8f6f63b77703fdcb7f7');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '3bb2a4da0981895013e0');
