3a99b63ec8a7ac3d4ce3ce96c1069b870406c0ff
Brad Vincent <bradvin@gmail.com>
2026-02-19 17:11:13 +0000

    build

--- build/scripts/admin.asset.php
+++ build/scripts/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'b5fc4c9e41af85d0411b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '8a36b4517dee2501395d');
