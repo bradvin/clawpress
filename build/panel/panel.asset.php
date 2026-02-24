0e9ec51448987cda59fea8dcf0071119b02e9bfc
Brad Vincent <bradvin@gmail.com>
2026-02-24 12:21:23 +0000

    Panel: cycle thinking dots as ., .., ...

--- build/panel/panel.asset.php
+++ build/panel/panel.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '1108363da715a2d672e7');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'd70a183e8e77d07cf475');
