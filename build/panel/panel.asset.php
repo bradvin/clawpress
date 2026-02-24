befeae7262e2659a01237549e782419c8c4889cd
Brad Vincent <bradvin@gmail.com>
2026-02-24 11:40:53 +0000

    new build

--- build/panel/panel.asset.php
+++ build/panel/panel.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '80b5e6a1d154a9389ca4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'a390177593ea24ac9b9a');
