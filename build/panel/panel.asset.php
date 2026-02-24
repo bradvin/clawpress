d12a9e5834fdcd8b5c4d267605162560ffe8c844
Brad Vincent <bradvin@gmail.com>
2026-02-24 12:44:43 +0000

    Refocus chat textarea after stream completes

--- build/panel/panel.asset.php
+++ build/panel/panel.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '3c8712ac26758b062f81');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '7e3d310fd29b6a495cec');
--- src/panel/Panel.jsx
+++ src/panel/Panel.jsx
@@ -1177,6 +1177,7 @@ const Panel = () => {
 					input={ input }
 					onInputChange={ ( e ) => setInput( e.target.value ) }
 					onSend={ sendPrompt }
+					panelOpen={ open }
 					suggestions={ suggestions }
 					contextUsage={ contextUsage }
 					onSendSuggestion={ ( suggestion ) =>
